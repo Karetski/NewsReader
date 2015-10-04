0ac408dc22b410b59282afe6ef84e1f8451ef9d0
Karetski <karetski@gmail.com>
2015-10-05 02:55:31 +0300

    NRRSSParserDelegate upgraded

--- News Reader/NRRSSParser.swift
+++ News Reader/NRRSSParser.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 protocol NSRSSParserDelegate {
-    func parsingWasFinished(channel: NRChannel)
+    func parsingWasFinished(channel: NRChannel?, error: NSError?)
 }
 
-class NRRSSParser: NSObject {
+class NRRSSParser: NSObject, NSXMLParserDelegate {
     var channel: NRChannel = NRChannel()
+    
+    var delegate: NSRSSParserDelegate?
+    
+    func startParsingWithData(data: NSData) {
+        let parser = NSXMLParser(data: data)
+        parser.delegate = self
+        parser.parse()
+    }
+    
+    // MARK: NSXMLParserDelegate implementation
+    
+    func parserDidEndDocument(parser: NSXMLParser) {
+        self.delegate?.parsingWasFinished(self.channel, error: nil)
+    }
+    
+    func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+        self.delegate?.parsingWasFinished(nil, error: parseError)
+    }
 }
