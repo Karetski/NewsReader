9524d7afcfae57eb04ea4e1c93ec0b84af685640
Karetski <karetski@gmail.com>
2015-11-19 00:59:35 +0300

    DetailImageView added + Code cleaning

---
+++ News Reader/DetailImageNewsCell.swift
@@ -0,0 +1,13 @@
+//
+//  DetailImageNewsCell.swift
+//  News Reader
+//
+//  Created by Alexey on 19.11.15.
+//  Copyright © 2015 Alexey. All rights reserved.
+//
+
+import Cocoa
+
+class DetailImageNewsCell: DetailNewsCell {
+
+}
